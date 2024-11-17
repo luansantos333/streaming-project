4247ccfbc7f0797f6707d9e1c48485b9df36c76a
luangomes <luangomespctu@gmail.com>
2024-11-17 20:20:00 -0300

    feat: add projection classes

---
+++ src/main/java/org/portfolio/streaming/repositories/projections/MovieGenreProjection.java
@@ -0,0 +1,21 @@
+package org.portfolio.streaming.repositories.projections;
+
+import java.time.Instant;
+
+public interface MovieGenreProjection {
+    Long getId();
+
+    String getDescription();
+
+    String getTitle();
+
+    String getDirector();
+
+    Instant getRelease();
+
+    Double getPrice();
+
+    Long getGenreId();
+
+    String getGenreName();
+}
