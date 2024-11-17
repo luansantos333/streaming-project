a59eb606554ea749634031dc8dfd1705390b321f
luangomes <luangomespctu@gmail.com>
2024-11-17 20:20:22 -0300

    feat: add searchMovieAndCategoriesById method

--- src/main/java/org/portfolio/streaming/repositories/MovieRepository.java
+++ src/main/java/org/portfolio/streaming/repositories/MovieRepository.java
@@ -1,7 +1,19 @@
 package org.portfolio.streaming.repositories;
 
 import org.portfolio.streaming.entities.Movie;
+import org.portfolio.streaming.repositories.projections.MovieGenreProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MovieRepository extends JpaRepository <Movie, Long> {
+
+
+    @Query(nativeQuery = true, value = "SELECT movie.id, movie.title, movie.description, movie.director, movie.release, movie.price, genre.id AS genre_id, genre.name AS genre_name " +
+            "FROM tb_movie AS movie INNER JOIN tb_movie_genre ON movie.id = tb_movie_genre.movie_id INNER JOIN tb_genre AS genre ON genre.id = tb_movie_genre.genre_id WHERE movie.id = :id"
+    )
+    List<MovieGenreProjection> searchMovieAndCategoriesById(Long id);
+
+
 }
