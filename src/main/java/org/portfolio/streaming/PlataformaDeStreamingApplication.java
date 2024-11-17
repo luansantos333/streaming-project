76d539c87708a5a54ae7a40515ebdd656e9683ea
luangomes <luangomespctu@gmail.com>
2024-11-17 10:00:49 -0300

    Innitial commit

---
+++ src/main/java/org/portfolio/streaming/PlataformaDeStreamingApplication.java
@@ -0,0 +1,13 @@
+package org.portfolio.streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaDeStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaDeStreamingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/portfolio/streaming/PlataformaDeStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.portfolio.streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaDeStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
