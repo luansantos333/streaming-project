91eb6e06ac9704ffd98fd655e1d86eddbc2ed6c2
luangomes <luangomespctu@gmail.com>
2024-11-19 19:51:09 -0300

    feat: add method searchReviewsByMovieId

--- src/main/java/org/portfolio/streaming/repositories/ReviewRepository.java
+++ src/main/java/org/portfolio/streaming/repositories/ReviewRepository.java
@@ -1,7 +1,20 @@
 package org.portfolio.streaming.repositories;
 
 import org.portfolio.streaming.entities.Review;
+import org.portfolio.streaming.repositories.projections.UserReviewProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ReviewRepository extends JpaRepository <Review, Long> {
+
+
+    @Query(nativeQuery = true, value = "SELECT user_sys.id AS userId, user_sys.email AS username, review.review, review.rating FROM tb_review AS review INNER JOIN "
+            + "tb_user AS user_sys ON review.user_id = user_sys.id INNER JOIN tb_movie AS movie ON review.movie_id = movie.id WHERE movie.id = :movieId")
+        public List<UserReviewProjection> searchReviewsByMovieId(@Param("movieId") Long movieId);
+
+
+
 }
