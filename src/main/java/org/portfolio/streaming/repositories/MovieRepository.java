2d32c7d103f2319a70594ed473ac6b6ce36d4d03
luangomes <luangomespctu@gmail.com>
2024-11-19 19:52:58 -0300

    fix: select imgUrl

--- src/main/java/org/portfolio/streaming/repositories/MovieRepository.java
+++ src/main/java/org/portfolio/streaming/repositories/MovieRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface MovieRepository extends JpaRepository <Movie, Long> {
 
 
-    @Query(nativeQuery = true, value = "SELECT movie.id, movie.title, movie.description, movie.director, movie.release, movie.price, genre.id AS genre_id, genre.name AS genre_name " +
+    @Query(nativeQuery = true, value = "SELECT movie.id, movie.title, movie.description, movie.director, movie.release, movie.price, movie.img_url AS imgUrl, genre.id AS genre_id, genre.name AS genre_name " +
             "FROM tb_movie AS movie INNER JOIN tb_movie_genre ON movie.id = tb_movie_genre.movie_id INNER JOIN tb_genre AS genre ON genre.id = tb_movie_genre.genre_id WHERE movie.id = :id"
     )
     List<MovieGenreProjection> searchMovieAndCategoriesById(Long id);
