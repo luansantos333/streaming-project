734f848cd97697334931fafcddf0a4b355951554
luangomes <luangomespctu@gmail.com>
2024-11-22 19:38:24 -0300

    feat: add review repository

--- src/main/java/org/portfolio/streaming/services/ReviewService.java
+++ src/main/java/org/portfolio/streaming/services/ReviewService.java
@@ -1,7 +1,14 @@
 package org.portfolio.streaming.services;
 
+import org.portfolio.streaming.repositories.ReviewRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewService {
+    private final ReviewRepository reviewRepository;
+
+
+    public ReviewService(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
 }
