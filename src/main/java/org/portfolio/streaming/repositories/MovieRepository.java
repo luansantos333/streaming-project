a49dd12c5f8e1194dd70a89866276514535b1897
luangomes <luangomespctu@gmail.com>
2024-11-24 10:23:33 -0300

    feat: add new query for getting all movies

--- src/main/java/org/portfolio/streaming/repositories/MovieRepository.java
+++ src/main/java/org/portfolio/streaming/repositories/MovieRepository.java
@@ -2,6 +2,8 @@ package org.portfolio.streaming.repositories;
 
 import org.portfolio.streaming.entities.Movie;
 import org.portfolio.streaming.repositories.projections.MovieGenreProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -14,6 +16,16 @@ public interface MovieRepository extends JpaRepository <Movie, Long> {
             "FROM tb_movie AS movie INNER JOIN tb_movie_genre ON movie.id = tb_movie_genre.movie_id INNER JOIN tb_genre AS genre ON genre.id = tb_movie_genre.genre_id WHERE movie.id = :id"
     )
     List<MovieGenreProjection> searchMovieAndCategoriesById(Long id);
+
+    @Query ("SELECT movie.id FROM Movie movie WHERE UPPER(movie.title) LIKE UPPER (CONCAT('%', :name, '%'))")
+    List<Long> searchMoviesById(String name);
+
+
+    @Query(nativeQuery = true, value = "SELECT * FROM (SELECT movie.id, movie.title, movie.release, movie.director, movie.img_url AS imgUrl, movie.price, genre.id AS genreId, genre.name AS genre_name " +
+            "FROM tb_movie AS movie INNER JOIN tb_movie_genre AS movie_genre ON movie.id = movie_genre.movie_id INNER JOIN tb_genre AS genre ON genre.id = movie_genre.genre_id WHERE movie.id IN (:movieIds)) AS selection", countQuery = "SELECT COUNT (*) FROM (SELECT movie.id, movie.title, movie.release, movie.director, movie.img_url AS imgUrl, movie.price, genre.id AS genreId, genre.name AS genre_name " +
+            "FROM tb_movie AS movie INNER JOIN tb_movie_genre AS movie_genre ON movie.id = movie_genre.movie_id INNER JOIN tb_genre AS genre ON genre.id = movie_genre.genre_id WHERE movie.id IN (:movieIds)) AS selection")
+    Page<MovieGenreProjection> searchAllMoviesAndGenresByMovieIds(List<Long> movieIds, Pageable p);
+
 
 
 }
