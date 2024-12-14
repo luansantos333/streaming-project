1fd5c77a73340ebfdbc6bf3b0a24c45f71b27b66
luangomes <luangomespctu@gmail.com>
2024-12-14 18:55:01 -0300

    feat: add ForbiddenException

--- src/main/java/org/portfolio/streaming/controllers/handlers/GlobalExceptionHandler.java
+++ src/main/java/org/portfolio/streaming/controllers/handlers/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ package org.portfolio.streaming.controllers.handlers;
 import jakarta.servlet.http.HttpServletRequest;
 import org.portfolio.streaming.controllers.handlers.dtos.DefaultErrorDTO;
 import org.portfolio.streaming.services.exceptions.DatabaseException;
+import org.portfolio.streaming.services.exceptions.ForbiddenException;
 import org.portfolio.streaming.services.exceptions.ResourceNotFoundException;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -31,6 +32,17 @@ public class GlobalExceptionHandler {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         DefaultErrorDTO err = new DefaultErrorDTO(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
+
+    }
+
+    @ExceptionHandler (ForbiddenException.class)
+    public ResponseEntity<DefaultErrorDTO> forbiddenAccessHandler (ForbiddenException e, HttpServletRequest request) {
+
+        HttpStatus status = HttpStatus.FORBIDDEN;
+        DefaultErrorDTO err = new DefaultErrorDTO(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+
 
     }
 
