56765def0ccac1ae49f0a9ef84a97e5d8e40e784
luangomes <luangomespctu@gmail.com>
2024-12-14 18:56:37 -0300

    fix: minor fixes

--- src/main/java/org/portfolio/streaming/repositories/ReviewRepository.java
+++ src/main/java/org/portfolio/streaming/repositories/ReviewRepository.java
@@ -14,10 +14,10 @@ public interface ReviewRepository extends JpaRepository <Review, Long> {
 
     @Query(nativeQuery = true, value = "SELECT user_sys.id AS userId, user_sys.email AS username, review.review, review.rating FROM tb_review AS review INNER JOIN "
             + "tb_user AS user_sys ON review.user_id = user_sys.id INNER JOIN tb_movie AS movie ON review.movie_id = movie.id WHERE movie.id = :movieId")
-        public List<UserReviewProjection> searchReviewsByMovieId(@Param("movieId") Long movieId);
+         List<UserReviewProjection> searchReviewsByMovieId(@Param("movieId") Long movieId);
 
     @Query(nativeQuery = true, value = "SELECT review.review, review.rating, usuario.email AS username FROM tb_review AS review INNER JOIN tb_user AS usuario ON review.user_id = usuario.id WHERE review.id = :id")
-    public Optional<UserReviewProjection> searchReviewAndUserByReviewId(Long id);
+     Optional<UserReviewProjection> searchReviewAndUserByReviewId(Long id);
 
 
 
